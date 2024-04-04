08379b9f6acf51c2b2991e91f73e10e860d97206
litlighilit <litlighilit@foxmail.com>
2024-04-04 18:18:55 +0800

    adjust to support serveless of vercel

--- api/srv.py
+++ api/srv.py
@@ -2,11 +2,15 @@
 import http.server
 import socketserver
 
+import sys
+import os.path
 from os import getenv
 from urllib.parse import urlparse, parse_qs
 
     
 from time import sleep
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 import cal
 
@@ -20,25 +24,21 @@ def inter():
       10
     )
 
-# listen on:
-env_port = getenv('MOONS_SRV_PORT')
-if env_port is None:
-    port = 3333
-else:
-    port = int(env_port)
+SKIP_PARAMS = {
+        "vercelToolbarCode"
+        }
 
-address = ('', port)
-
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+class handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         fullpath = self.path
         url = urlparse(fullpath)
-        path = url.path[1:]  # skip '/'
+        path = url.path.rsplit('/', 1)[1]  # skip '/'
+        if path == 'api': path = ''
         query_string = url.query
         # parse to get dict
         params = parse_qs(query_string)
         # `parse_qs`'s value is always `list`
-        params_dict = {k: v[0] if len(v) == 1 else v for k, v in params.items()}
+        params_dict = {k: v[0] if len(v) == 1 else v for k, v in params.items() if k not in SKIP_PARAMS}
 
         client_ip = self.client_address[0]
         try:
@@ -47,6 +47,10 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         except cal.IpLookUpError as e:
             res = cal.BAD_REQ_RES(e, "ip-loc lookup failed")
             status = 400
+        except TypeError as e:
+            res = cal.BAD_REQ_RES(e, "axtra/invalid params given")
+            status = 400
+
 
         # set header
         self.send_response(status)
@@ -57,7 +61,16 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         inter()
 
 
-with socketserver.TCPServer(address, MyHttpRequestHandler) as httpd:
-    print(f"Serving at port {port}...")
-    httpd.serve_forever()
+if __name__ == '__main__':
+    # listen on:
+    env_port = getenv('MOONS_SRV_PORT')
+    if env_port is None:
+        port = 3333
+    else:
+        port = int(env_port)
+
+    address = ('', port)
+    with socketserver.TCPServer(address, handler) as httpd:
+        print(f"Serving at port {port}...")
+        httpd.serve_forever()
 
